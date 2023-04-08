10998e533980d6afbf7e97311064a3df4a57edab
Wallace Júnior <wflj1997@gmail.com>
2023-04-08 12:18:31 -0300

    Setup do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "~/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-900 text-zinc-100">
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
+    screens: {
+      xs: "425px",
+      ...defaultTheme.screens,
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
+};
